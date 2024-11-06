25410efcf6dcb2e3234d41b0d79c481ec58d7446
naamiraa <naamiraaulia@gmail.com>
2024-11-06 09:57:52 +0700

    menambahkan card nama, npm, kelas

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
-
 class MyHomePage extends StatelessWidget {
+  final String npm = '2306165931'; // NPM
+  final String name = 'Namira Aulia'; // Nama
+  final String className = 'PBP C'; // Kelas
   MyHomePage({super.key});
-
   // Daftar item dengan warna berbeda
   final List<ItemHomepage> items = [
-    ItemHomepage("Lihat Daftar Produk", Icons.shopping_bag_outlined, const Color.fromARGB(255, 171, 129, 110)), // Warna cokelat tanah
-    ItemHomepage("Tambah Produk", Icons.add, const Color.fromARGB(255, 111, 96, 95)), // Warna hijau tua
-    ItemHomepage("Logout", Icons.logout, const Color.fromARGB(255, 89, 36, 36)), // Warna merah bata
-  ];
-
+         ItemHomepage("Lihat Daftar Produk", Icons.shopping_bag_outlined, const Color.fromARGB(255, 111, 82, 71)), // Warna cokelat tanah
+         ItemHomepage("Tambah Produk", Icons.add, const Color.fromARGB(255, 128, 88, 61)), // Warna hijau tua
+         ItemHomepage("Logout", Icons.logout, const Color.fromARGB(255, 89, 36, 36)), // Warna merah bata
+     ];
   @override
   Widget build(BuildContext context) {
     // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
     return Scaffold(
       // AppBar adalah bagian atas halaman yang menampilkan judul.
       appBar: AppBar(
-        // Judul aplikasi "Heritage Craft" dengan teks putih dan tebal.
+        // Judul aplikasi "Mental Health Tracker" dengan teks putih dan tebal.
         title: const Text(
-          'Heritage Craft',
+          'Heritage Craft E-Commerce',
           style: TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
           ),
         ),
         // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
-        backgroundColor: Theme.of(context).colorScheme.secondary,
+        backgroundColor: Theme.of(context).colorScheme.primary,
       ),
       // Body halaman dengan padding di sekelilingnya.
       body: Padding(
@@ -34,9 +34,17 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            // Row untuk menampilkan 3 InfoCard secara horizontal.
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                InfoCard(title: 'NPM', content: npm),
+                InfoCard(title: 'Name', content: name),
+                InfoCard(title: 'Class', content: className),
+              ],
+            ),
             // Memberikan jarak vertikal 16 unit.
             const SizedBox(height: 16.0),
-
             // Menempatkan widget berikutnya di tengah halaman.
             Center(
               child: Column(
@@ -53,7 +61,6 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                   ),
-
                   // Grid untuk menampilkan ItemCard dalam bentuk grid 3 kolom.
                   GridView.count(
                     primary: true,
@@ -63,7 +70,6 @@ class MyHomePage extends StatelessWidget {
                     crossAxisCount: 3,
                     // Agar grid menyesuaikan tinggi kontennya.
                     shrinkWrap: true,
-
                     // Menampilkan ItemCard untuk setiap item dalam list items.
                     children: items.map((ItemHomepage item) {
                       return ItemCard(item);
@@ -78,16 +84,11 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-
-
 class InfoCard extends StatelessWidget {
   // Kartu informasi yang menampilkan title dan content.
-
   final String title;  // Judul kartu.
   final String content;  // Isi kartu.
-
   const InfoCard({super.key, required this.title, required this.content});
-
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -112,22 +113,17 @@ class InfoCard extends StatelessWidget {
     );
   }
 }
-
 class ItemHomepage {
   final String name;
   final IconData icon;
   final Color color; // Tambahkan atribut warna untuk setiap item
-
   ItemHomepage(this.name, this.icon, this.color);
 }
-
 class ItemCard extends StatelessWidget {
   // Menampilkan kartu dengan ikon dan nama.
-
-  final ItemHomepage item;
-
-  const ItemCard(this.item, {super.key});
-
+  final ItemHomepage item; 
+  
+  const ItemCard(this.item, {super.key}); 
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -135,6 +131,7 @@ class ItemCard extends StatelessWidget {
       color: item.color, // Gunakan warna khusus dari item
       // Membuat sudut kartu melengkung.
       borderRadius: BorderRadius.circular(12),
+      
       child: InkWell(
         // Aksi ketika kartu ditekan.
         onTap: () {
@@ -171,4 +168,5 @@ class ItemCard extends StatelessWidget {
       ),
     );
   }
+  
 }
